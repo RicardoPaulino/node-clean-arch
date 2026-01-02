82e942ff139d531713dace9099d2ab340425da55
Ricardo Paulino <ricardo.paulino@byteon.com.br>
2026-01-01 22:31:30 -0300

    Created email validator test

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -11,10 +11,32 @@ describe('Email Validator', () => {
     const isEmailValid = sut.isValid('valid_email@gmail.com')
     expect(isEmailValid).toBe(true)
   })
+
   test('should return false if validator returns false', () => {
     const sut = new EmailValidator()
     validator.isEmailValid = false
     const isEmailValid = sut.isValid('invalid_email.com')
+    expect(isEmailValid).toBe(false)
+  })
+
+  test('should call validator with correct email', () => {
+    const sut = new EmailValidator()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('valid_email@gmail.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('valid_email@gmail.com')
+  })
+
+  test('should throw if validator throws', () => {
+    const sut = new EmailValidator()
+    jest.spyOn(validator, 'isEmail').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    expect(() => sut.isValid('invalid_email.com')).toThrow()
+  })
+
+  test('should return false if email is empty', () => {
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('')
     expect(isEmailValid).toBe(false)
   })
 })
