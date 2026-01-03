e96a5d26b24e7b77c7d6c30408b35ec63f0511b0
Ricardo Paulino <ricardo.paulino@byteon.com.br>
2026-01-03 13:36:04 -0300

    Should return MissingParamError if no parameters are provided

--- domain/usecases/auth-usecase.spec.js
+++ domain/usecases/auth-usecase.spec.js
@@ -1,3 +1,5 @@
+const { MissingParamError } = require('../../src/presentation/errors')
+
 class AuthUseCase {
   constructor () {
     this.accessToken = 'valid_token'
@@ -7,6 +9,9 @@ class AuthUseCase {
     if (!email || !password) {
       this.accessToken = null
     }
+    if (!email && !password) {
+      return new MissingParamError('email or password is missing')
+    }
     return this.accessToken
   }
 }
@@ -14,13 +19,40 @@ class AuthUseCase {
 describe('Auth UseCase', () => {
   test('Should return null if no email is provider', async () => {
     const sut = new AuthUseCase()
-    const accessToken = await sut.auth({ email: null, password: 'any_password' })
+    const accessToken = await sut.auth({
+      email: null,
+      password: 'any_password'
+    })
     expect(accessToken).toBeNull()
   })
 
   test('Should return null if no password is provider', async () => {
     const sut = new AuthUseCase()
-    const accessToken = await sut.auth({ email: 'any_email@gmail.com', password: null })
+    const accessToken = await sut.auth({
+      email: 'any_email@gmail.com',
+      password: null
+    })
     expect(accessToken).toBeNull()
+  })
+
+  test('Should return access token if email and password are provided', async () => {
+    const sut = new AuthUseCase()
+    const accessToken = await sut.auth({
+      email: 'any_email@gmail.com',
+      password: 'any_password'
+    })
+    expect(accessToken).toBe('valid_token')
+  })
+
+  test('Should throw if no dependencies are provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth()
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return MissingParamError if no parameters are provided', async () => {
+    const sut = new AuthUseCase()
+    const accessToken = await sut.auth({})
+    expect(accessToken).toEqual(new MissingParamError('email or password is missing'))
   })
 })
